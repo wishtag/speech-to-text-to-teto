bfabee8b249793c43d01f447ae62d7e1245ea155
HaydenPhillips777 <hlphillips1@ualr.edu>
2025-06-24 16:27:58 -0500

    Updated File Creation
    Now instead of having one set voice segment and having it fit the sentence into that, it creates a voice segment for each word in the input string, calculating the duration it should last based off of the word's length.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from RealtimeSTT import AudioToTextRecorder
-import pyttsx3
 import threading
 from pygame import mixer
 import os
@@ -10,21 +9,31 @@ import pyautogui
 
 mixer.init(devicename="CABLE Input (VB-Audio Virtual Cable)")
 
-engine = pyttsx3.init()
-engine.setProperty('rate', 200)
-
 def generate_random_string(length=8):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-def replace_in_file(output_file, search_phrase, replacement_string):
+def replace_in_file(output_file, replacement_string):
     try:
-        with open("template.ustx", 'r', encoding='utf-8') as file:
-            content = file.read()
+        words = replacement_string.split() #splits the string into a list of words
 
-        updated_content = content.replace(search_phrase, replacement_string)
+        with open("template.ustx", 'r', encoding='utf-8') as file: #the template for the basic stuff of the file
+            file_template = file.read()
+
+        with open("word.txt", 'r', encoding='utf-8') as file: #the template for adding a new voice segment
+            speech_template = file.read()
 
         with open(f"temp\{output_file}", 'w', encoding='utf-8') as output_file:
-            output_file.write(updated_content)
+            total_dur = 0 #the total duration of the file, as this number is updated it will be used to figure out the position of each segment
+            output_file.write(file_template) #adds basic file stuff
+            for i in range(len(words)): #loops through the list of words
+                segment = speech_template #copies the template so we dont accidently override the original
+                segment = segment.replace("POS__", str(total_dur))
+                duration = len(words[i])*75 #calculates a duration for how long the word should be said based off of the length of the word
+                segment = segment.replace("DUR__", str(duration))
+                total_dur = total_dur + duration
+                segment = segment.replace("WORD__", words[i])
+                output_file.write("\n"+segment) #adds new segment
+            output_file.write("\nwave_parts: []") #finalizes file by adding stuff that comes after the segments
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -33,13 +42,11 @@ if __name__ == '__main__':
     print("Wait until it says 'speak now'")
 
     recorder = AudioToTextRecorder(language="en")
-
-    search_phrase = "REPLACE_ME"
     
     while True:
         text = recorder.text()
         print(text)
         output_file = f'{generate_random_string()}.ustx'
-        replace_in_file(output_file, search_phrase, text)
+        replace_in_file(output_file, text)
         #t = threading.Thread(target=replace_in_file, args=(output_file, search_phrase, text,))
         #t.start()
--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-import pyautogui
-import time
-
-time.sleep(1)
-window = pyautogui.getWindowsWithTitle("OpenUtau")[0]
-window.activate()
-pyautogui.click(window.left + 10, window.top + 10)
-time.sleep(1)
-window = pyautogui.getWindowsWithTitle("Roblox")[0]
-window.activate()
-pyautogui.click(window.left + 10, window.top + 10)
+val = "Hey guys, how is it going?"
+vals = val.split()
+for i in range(len(vals)):
+    print(f"{vals[i]}: {len(vals[i])}")
